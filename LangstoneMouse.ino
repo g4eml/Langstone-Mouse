51592f80f573243be67942a2ea5102c87a284f53
g4eml <colin@durbridge.me.uk>
2020-10-25 15:36:07 +0000

    Add third button

--- LangstoneMouse.ino
+++ LangstoneMouse.ino
@@ -1,8 +1,8 @@
-// Rotary Encoder and Button Mouse Emulation for Langstone Transceiver. By Colin DurightButtonReleasedidge G4EML
+// Rotary Encoder and Button Mouse Emulation for Langstone Transceiver. By Colin Durbridge G4EML
 // for Arduino Pro Micro Board. (5V 16MHz Version) (won't work with all Arduinos. Needs the ATmega32U4 chip for USB Mouse emulation.  
 // Needs the Encoder Library by Paul Stoffregen. (Search for Encoder in Library Manager)
-// Rotary encoder (used as tuning knob) is read and scaled to suitable output pulses per revolution then sent as mouse scroll wheel movement every 10 ms.
-// 2 Buttons (used to select tuning rate) are read and sent as mouse clicks on the left and right mouse buttons. 
+// Rotary encoder (used as tuning knob) is read and scaled to suitable output pulses per revolution then sent as mouse scroll wheel movement every 20 ms.
+// 3 Buttons (used to select tuning rate and dial lock) are read and sent as mouse clicks on the left, middle and right mouse buttons. 
 //
 
 #include <Mouse.h>
@@ -18,16 +18,19 @@
 
 
 
-#define RPHA 2                        //Rotary encoder A phase connected to Pin 2. Encoder Common connection to ground.  (do not change, needs the interrupt on this pin to work properly)
-#define RPHB 3                        //Rotary encoder B phase connected to Pin 3.                                     
-#define LEFTBUTTON 4                //Left and right push buttons. Used by Langstone to select the tuning rate. 
-#define RIGHTBUTTON 5               //Connect buttons between these two pins and ground. 
+#define RPHA 3                        //Rotary encoder A phase connected to Pin 2. Encoder Common connection to ground.  (do not change, needs the interrupt on this pin to work properly)
+#define RPHB 2                        //Rotary encoder B phase connected to Pin 3. Swap these two pins to reverse the encoder direction.                                    
+#define LEFTBUTTON 4                //Left, right and middle push buttons. Used by Langstone to select the tuning rate and dial lock. 
+#define RIGHTBUTTON 5               //Connect buttons between these three pins and ground. 
+#define MIDDLEBUTTON 6
 
 int encoderDiv;
 int leftButton;
 int rightButton;
+int middleButton;
 int leftButtonReleased;
 int rightButtonReleased;
+int middleButtonReleased;
 
 Encoder Enc(RPHA, RPHB);
 
@@ -36,22 +39,28 @@ void setup()
  encoderDiv=(encoderStepsPerRev*4)/outputStepsPerRev;                     // calculate the requred encoder divisor. (Encoder library outputs 4 steps per input step)
  pinMode(LEFTBUTTON,INPUT_PULLUP);
  pinMode(RIGHTBUTTON,INPUT_PULLUP);
+ pinMode(MIDDLEBUTTON,INPUT_PULLUP);
  leftButtonReleased=false;
  rightButtonReleased=false; 
+ middleButtonReleased=false; 
  Mouse.begin();
 }
 
 void loop() 
 {
-  delay(20);                            //delay to slow down the rate of USB messages. 50 per second is plenty. 
-long counts = Enc.read()/encoderDiv;
+delay(20);                              //delay to slow down the rate of USB messages. 50 per second is plenty. 
+long counts = Enc.read()/encoderDiv;    //number of encoder counts since last sent to USB.
+
 if(counts!=0)
   {
   Mouse.move(0,0,counts);
-  Enc.write(0);  
+  Enc.write(0);                         //reset the encoder counts
   }
+  
   leftButton=digitalRead(LEFTBUTTON);
   rightButton=digitalRead(RIGHTBUTTON);
+  middleButton=digitalRead(MIDDLEBUTTON);
+
   if(leftButton==LOW)
     {
      if(leftButtonReleased>=3)
@@ -64,6 +73,8 @@ if(counts!=0)
     {
       if(leftButtonReleased<3) leftButtonReleased=leftButtonReleased+1;
     }
+
+    
   if(rightButton==LOW)
     {
      if(rightButtonReleased>=3)
@@ -75,6 +86,20 @@ if(counts!=0)
   else
     {
       if(rightButtonReleased<3) rightButtonReleased=rightButtonReleased+1;
+    }
+
+
+  if(middleButton==LOW)
+    {
+     if(middleButtonReleased>=3)
+      {
+       Mouse.click(MOUSE_MIDDLE);  
+       middleButtonReleased=false;
+      }
+    }
+  else
+    {
+      if(middleButtonReleased<3) middleButtonReleased=middleButtonReleased+1;
     }
 
 }
